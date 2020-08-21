550f24fe0f90658dbe460551b2c79a56ce907c83
Vera <uamailcom@gmail.com>
2020-08-21 08:55:59 +0300

    Menu, Auth Localize Library Added

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the users. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'home'              =>'Home',
+    'about_us'          =>'About us',
+    'listings'          =>'Listings',
+    'news'              =>'News',
+    'contact'           =>'Contact',
+    'submit_listing'    =>'submit listing'
+];
---
+++ resources/lang/ru/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the users. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ці облікові дані не відповідають нашим даним.',
+    'throttle' => 'Занадто багато спроб входу. Спробуйте ще раз :seconds секунд.',
+    'login' =>"Логін",
+    'register' => "Реєстрація",
+    'my_account' => "Мій Аккаунт",
+    'logout' => 'Вийти',
+
+];
